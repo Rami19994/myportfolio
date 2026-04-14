1e3a759aba7c25996d8a08696d455365348727bb
Rami Rkab <138163686+Rami19994@users.noreply.github.com>
2026-04-14 16:11:53 +0300

    Update models.py

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,5 @@
-from django.utils.text import slugify
 from django.db import models
+from django.utils.text import slugify
 from ckeditor.fields import RichTextField
 from core.models import TechStack
 
@@ -11,9 +11,13 @@ class FeaturedProjectManager(models.Manager):
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
     image = models.ImageField(upload_to="project_images/", blank=True, null=True)
-    tech_stacks = models.ManyToManyField(TechStack, related_name="projects", blank=True)
+    tech_stacks = models.ManyToManyField(
+        TechStack,
+        related_name="projects",
+        blank=True,
+    )
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
     description = RichTextField()
@@ -21,15 +25,20 @@ class Project(models.Model):
     source_code = models.URLField(blank=True, null=True)
     featured = models.BooleanField(default=False)
 
-    objects = models.Manager()  # Default manager
-    featured_projects = FeaturedProjectManager()  # Custom manager for featured projects
+    objects = models.Manager()
+    featured_projects = FeaturedProjectManager()
 
-    @property
-    def is_open_source(self):
-        return self.source_code is not None and self.source_code != ""
+    class Meta:
+        ordering = ["-id"]
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
 
     def __str__(self):
         return self.name
+
+    @property
+    def is_open_source(self):
+        return bool(self.source_code)
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -40,8 +49,15 @@ class Project(models.Model):
 class ProjectKeyFeature(models.Model):
     short_description = models.CharField(max_length=255)
     project = models.ForeignKey(
-        Project, related_name="key_features", on_delete=models.CASCADE
+        Project,
+        related_name="key_features",
+        on_delete=models.CASCADE,
     )
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Project Key Feature"
+        verbose_name_plural = "Project Key Features"
 
     def __str__(self):
         return self.short_description
